6438faa817019b5fe05126cba41eb99237509736
Kat Doyle <Katrina.Doyle@digital.homeoffice.gov.uk>
2025-08-07 15:40:01 +0100

    updating-round-8-routing

--- app/routes.js
+++ app/routes.js
@@ -642,6 +642,21 @@ router.post('/beta/round8/triage/triage-question-3', function(request, response)
 
 // ------------------------- BETA triage round 8 END ----------------------------//
 
+// ------------------------- BETA Routing question page for mix of cert types round 8 END ----------------------------//
+router.post('/beta/round8/death/add-another-item-copy', function(request, response) {
+
+	var addanother = request.session.data['addanother']
+	if ( addanother == "birth"){
+		response.redirect("home-page-birth")
+	} 
+    else if ( addanother == "death"){
+		response.redirect("home-page-death.html")
+	} 
+    else {
+		response.redirect("basket.html")
+	}
+})
+
 
 // BETA JS
 
